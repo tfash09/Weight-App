3943d741fae2ef72fb535b2fe1fca6265d012aad
Tunde Siyanbola <tunde.siyanbola@azenia.com>
2022-12-23 08:51:56 +0100

    remove warnings

--- src/components/historyWeightChart/index.js
+++ src/components/historyWeightChart/index.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react'
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, Tooltip, ResponsiveContainer } from 'recharts';
 
 
 const data = [
